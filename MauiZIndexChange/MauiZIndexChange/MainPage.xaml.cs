0b1ce68dd98d5fd7af11aa167ffc3c1461bdddd2
dean-msft <defaizal@microsoft.com>
2022-02-18 12:42:35 -0500

    Initial project

---
+++ MauiZIndexChange/MauiZIndexChange/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace MauiZIndexChange;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnButtonClicked(object sender, EventArgs e)
+	{
+		var button = sender as Button;
+		var parentLayout = button.Parent as Layout;
+		parentLayout.ZIndex = 0;
+	}
+}
+
