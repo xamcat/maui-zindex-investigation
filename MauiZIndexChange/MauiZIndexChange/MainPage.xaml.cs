7a5a8a8987a14bc6024c26fab892e2701386db27
Dean Faizal <2334530+DeanFaizal@users.noreply.github.com>
2022-02-23 11:14:39 -0500

    Testing with Grid and VerticalStackLayouts

--- MauiZIndexChange/MauiZIndexChange/MainPage.xaml.cs
+++ MauiZIndexChange/MauiZIndexChange/MainPage.xaml.cs
@@ -1,19 +1,37 @@
-﻿namespace MauiZIndexChange;
+﻿
+namespace MauiZIndexChange;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    private List<VerticalStackLayout> _stackLayouts;
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+        _stackLayouts = new List<VerticalStackLayout>
+        {
+            Layout00,
+            Layout01,
+            Layout10,
+            Layout11
+        };
+    }
 
-	private void OnButtonClicked(object sender, EventArgs e)
-	{
-		var button = sender as Button;
-		var parentLayout = button.Parent as Layout;
-		parentLayout.ZIndex = 0;
-	}
+    private void OnButtonClicked(object sender, EventArgs e)
+    {
+        var button = sender as Button;
+        var parentLayout = button.Parent as VerticalStackLayout;
+
+        foreach (var layout in _stackLayouts)
+        {
+            if (layout == parentLayout)
+            {
+                layout.ZIndex = 0;
+            }
+            else
+            {
+                layout.ZIndex = 1;
+            }
+        }
+    }
 }
-
